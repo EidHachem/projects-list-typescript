56e87c61daf79fd5f85667496dea26a3cda9651a
EidHachem <eidhachem1@gmail.com>
2022-11-02 16:49:16 +0200

    Finish drag and drop feature

--- src/app.ts
+++ src/app.ts
@@ -59,8 +59,20 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active,
     );
     this.projects.push(newProject);
-    for (const listenFn of this.listeners) {
-      listenFn(this.projects.slice());
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
     }
   }
 }
@@ -199,9 +211,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
       listEl.classList.add('droppable');
     }
   }
-
+  @autoBind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished,
+    );
   }
   @autoBind
   dragLeaveHandler(_: DragEvent): void {
